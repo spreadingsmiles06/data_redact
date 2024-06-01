f317fcb27cf25d48fbd634aacb4f02baed3103c6
spreadingsmiles06 <137323381+spreadingsmiles06@users.noreply.github.com>
2024-06-01 09:01:18 +0530

    Update With_llm.py

--- Capstone_Project_Streamlit/With_llm.py
+++ Capstone_Project_Streamlit/With_llm.py
@@ -10,44 +10,38 @@ from pdfminer.high_level import extract_text
 from langchain_google_genai import ChatGoogleGenerativeAI
 import os
 import streamlit as st
+# genai.configure(api_key="AIzaSyDONU1LmKi9fVNEG9tsopZvRB0EcdKcfs8")'AIzaSyCNgcmnCCA81fPQWX_WL7Z786w-fOiankE'
+	# Get OpenAI API key from user
+gemini_api_key = st.text_input("Enter your GeminiAI API key:", type="password")
+if gemini_api_key:     
+    os.environ['GOOGLE_API_KEY'] =gemini_api_key
+    try:
+        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
+    except:
+        llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
+    # Initialize Presidio Analyzer and Anonymizer
+    analyzer = AnalyzerEngine()
+    anonymizer = AnonymizerEngine()
 
-gemini_api_key = st.text_input("Enter your Gemini API key:", type="password")
-import os
-os.environ["GEMINI_API_KEY"] = gemini_api_key
+    # Provide your OpenAI API key
+    #openai_api_key = "sk-XXXXX"
 
+    # Initialize OpenAI language model
+    # llm = OpenAI(temperature=0.7, openai_api_key=openGPT_key)
 
-# if gemini_api_key:
-# # Set the OpenAI API key
-#     import os
-#     os.environ["GEMINI_API_KEY"] = gemini_api_key
-# else:
-#     st.warning("Please enter your Gemini API  tokey continue.")
+    # Define the prompt template
+    demo_template = 'In the following text, identify and REDACT (replace with [REDACTED]) any sensitive information such as names, addresses, phone numbers, email addresses, Social Security numbers, and credit card , gender , age numbers you find in the {text}.'
 
-try:
-	llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
-except:
-	llm = ChatGoogleGenerativeAI(model="gemini-1.5-pro-latest")
-# Initialize Presidio Analyzer and Anonymizer
-analyzer = AnalyzerEngine()
-anonymizer = AnonymizerEngine()
+    # Set up the prompt template
+    prompt = PromptTemplate(
+        input_variables=['text'],
+        template=demo_template
+    )
 
-# Provide your OpenAI API key
-#openai_api_key = "sk-XXXXX"
-
-# Initialize OpenAI language model
-# llm = OpenAI(temperature=0.7, openai_api_key=openGPT_key)
-
-# Define the prompt template
-demo_template = 'In the following text, identify and REDACT (replace with [REDACTED]) any sensitive information such as names, addresses, phone numbers, email addresses, Social Security numbers, and credit card , gender , age numbers you find in the {text}.'
-
-# Set up the prompt template
-prompt = PromptTemplate(
-    input_variables=['text'],
-    template=demo_template
-)
-
-# Set up the LLM chain
-chain1 = LLMChain(llm=llm, prompt=prompt)
+    # Set up the LLM chain
+    chain1 = LLMChain(llm=llm, prompt=prompt)
+else:
+    st.warning("Please enter your OpenAI API key to continue.")
 
 # Function to redact PII from text
 def redact_pii(text):
@@ -84,7 +78,7 @@ def download_file(file_path):
     st.download_button(label="Download", data=contents, file_name=os.path.basename(file_path))
 
 # Set the directory path for PDF files
-directory_path = "Capstone_Project_Streamlit/pdf"
+directory_path = r"S:\HomeSmiles\JPythonF\ISBCapstoneJ\dataredaction\ISB_Project\pdf"
 
 def save_uploaded_files(uploaded_files, directory_path):
     # for uploaded_file in uploaded_files:
